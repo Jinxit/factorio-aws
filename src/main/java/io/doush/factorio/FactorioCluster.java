58c3144ae8bcb760bbef495ed702d1345ebebef3
Lucas Åström <lucasas@kth.se>
2020-03-06 10:34:34 +0100

    consistent maps (god damnit java)

--- src/main/java/io/doush/factorio/FactorioCluster.java
+++ src/main/java/io/doush/factorio/FactorioCluster.java
@@ -1,6 +1,5 @@
 package io.doush.factorio;
 
-import com.amazonaws.auth.policy.Principal;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
@@ -12,12 +11,10 @@ import software.amazon.awscdk.core.SecretsManagerSecretOptions;
 import software.amazon.awscdk.services.apigateway.LambdaIntegration;
 import software.amazon.awscdk.services.apigateway.MethodOptions;
 import software.amazon.awscdk.services.apigateway.RestApi;
-import software.amazon.awscdk.services.autoscaling.RollingUpdateConfiguration;
 import software.amazon.awscdk.services.codebuild.*;
 import software.amazon.awscdk.services.codepipeline.Artifact;
 import software.amazon.awscdk.services.codepipeline.Pipeline;
 import software.amazon.awscdk.services.codepipeline.StageOptions;
-import software.amazon.awscdk.services.codepipeline.StageProps;
 import software.amazon.awscdk.services.codepipeline.actions.CodeBuildAction;
 import software.amazon.awscdk.services.codepipeline.actions.CodeBuildActionType;
 import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
@@ -26,13 +23,16 @@ import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
 import software.amazon.awscdk.services.dynamodb.Table;
-import software.amazon.awscdk.services.ec2.*;
-import software.amazon.awscdk.services.ecr.LifecycleRule;
+import software.amazon.awscdk.services.ec2.IVpc;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
 import software.amazon.awscdk.services.ecr.Repository;
-import software.amazon.awscdk.services.ecs.AddCapacityOptions;
 import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ClusterAttributes;
-import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.LayerVersion;
@@ -42,10 +42,7 @@ import software.amazon.awscdk.services.route53.HostedZoneProviderProps;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.assets.AssetOptions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FactorioCluster extends Construct {
@@ -66,7 +63,7 @@ public class FactorioCluster extends Construct {
                         .build())
                 )
                 .handler("rcon.main")
-                .environment(Map.of(
+                .environment(Collections.singletonMap(
                         "DOMAIN_NAME", domainName
                 ))
                 .events(List.of())
@@ -194,7 +191,7 @@ public class FactorioCluster extends Construct {
                 .environment(BuildEnvironment.builder()
                         .computeType(ComputeType.SMALL)
                         .buildImage(LinuxBuildImage.STANDARD_3_0)
-                        .environmentVariables(Map.of(
+                        .environmentVariables(Collections.singletonMap(
                                 "FACTORIO_VERSION", BuildEnvironmentVariable.builder()
                                         .type(BuildEnvironmentVariableType.PLAINTEXT)
                                         .value("")
@@ -214,7 +211,7 @@ public class FactorioCluster extends Construct {
                 .environment(BuildEnvironment.builder()
                         .computeType(ComputeType.SMALL)
                         .buildImage(LinuxBuildImage.STANDARD_3_0)
-                        .environmentVariables(Map.of(
+                        .environmentVariables(Collections.singletonMap(
                                 "DOMAIN_NAME", BuildEnvironmentVariable.builder()
                                         .type(BuildEnvironmentVariableType.PLAINTEXT)
                                         .value(domainName)
@@ -256,8 +253,8 @@ public class FactorioCluster extends Construct {
 
         var oauthToken = SecretValue.secretsManager("FactorioCredentials",
                 SecretsManagerSecretOptions.builder()
-                .jsonField("GitHub")
-                .build());
+                        .jsonField("GitHub")
+                        .build());
 
         var artifactBucket = Bucket.Builder.create(this, "artifactBucket")
                 .removalPolicy(RemovalPolicy.DESTROY)
@@ -272,89 +269,90 @@ public class FactorioCluster extends Construct {
         codePipeline.getRole().grant(codeBuildDocker.getGrantPrincipal(), "*");
 
         codePipeline.addStage(StageOptions.builder()
-                                .stageName("Source")
-                                .actions(List.of(
-                                        GitHubSourceAction.Builder.create()
-                                                .actionName("Docker-Image")
-                                                .owner("Jinxit")
-                                                .repo("factorio-docker")
-                                                .trigger(GitHubTrigger.WEBHOOK)
-                                                .branch("master")
-                                                .variablesNamespace("factorio-docker-ns")
-                                                .output(Artifact.artifact("factorio-docker"))
-                                                .oauthToken(oauthToken)
-                                                .build(),
-                                        GitHubSourceAction.Builder.create()
-                                                .actionName("Install-Script")
-                                                .owner("Jinxit")
-                                                .repo("factorio-init")
-                                                .trigger(GitHubTrigger.WEBHOOK)
-                                                .branch("master")
-                                                .variablesNamespace("factorio-init-ns")
-                                                .output(Artifact.artifact("factorio-init"))
-                                                .oauthToken(oauthToken)
-                                                .build(),
-                                        GitHubSourceAction.Builder.create()
-                                                .actionName("CDK-Infrastructure")
-                                                .owner("Jinxit")
-                                                .repo("factorio-aws")
-                                                .trigger(GitHubTrigger.WEBHOOK)
-                                                .branch("master")
-                                                .variablesNamespace("factorio-aws-ns")
-                                                .output(Artifact.artifact("factorio-aws"))
-                                                .oauthToken(oauthToken)
-                                                .build()
-                                ))
+                .stageName("Source")
+                .actions(List.of(
+                        GitHubSourceAction.Builder.create()
+                                .actionName("Docker-Image")
+                                .owner("Jinxit")
+                                .repo("factorio-docker")
+                                .trigger(GitHubTrigger.WEBHOOK)
+                                .branch("master")
+                                .variablesNamespace("factorio-docker-ns")
+                                .output(Artifact.artifact("factorio-docker"))
+                                .oauthToken(oauthToken)
+                                .build(),
+                        GitHubSourceAction.Builder.create()
+                                .actionName("Install-Script")
+                                .owner("Jinxit")
+                                .repo("factorio-init")
+                                .trigger(GitHubTrigger.WEBHOOK)
+                                .branch("master")
+                                .variablesNamespace("factorio-init-ns")
+                                .output(Artifact.artifact("factorio-init"))
+                                .oauthToken(oauthToken)
+                                .build(),
+                        GitHubSourceAction.Builder.create()
+                                .actionName("CDK-Infrastructure")
+                                .owner("Jinxit")
+                                .repo("factorio-aws")
+                                .trigger(GitHubTrigger.WEBHOOK)
+                                .branch("master")
+                                .variablesNamespace("factorio-aws-ns")
+                                .output(Artifact.artifact("factorio-aws"))
+                                .oauthToken(oauthToken)
                                 .build()
+                ))
+                .build()
         );
 
         if (items.size() > 0) {
             codePipeline.addStage(StageOptions.builder()
-                                    .stageName("Build")
-                                    .actions(items.stream().map(server -> CodeBuildAction.Builder.create()
-                                            .actionName(server.get("version").getS())
-                                            .project(codeBuildDocker)
-                                            .input(Artifact.artifact("factorio-docker"))
-                                            .type(CodeBuildActionType.BUILD)
-                                            .variablesNamespace("factorio-" + server.get("version")
-                                                    .getS().replaceAll("\\.", "_"))
-                                            .environmentVariables(Map.of(
-                                                    "FACTORIO_VERSION", BuildEnvironmentVariable.builder()
-                                                            .type(BuildEnvironmentVariableType.PLAINTEXT)
-                                                            .value(server.get("version").getS())
-                                                            .build(),
-                                                    "IMAGE_REPO_NAME", BuildEnvironmentVariable.builder()
-                                                            .type(BuildEnvironmentVariableType.PLAINTEXT)
-                                                            .value(ecrRepo.getRepositoryName())
-                                                            .build(),
-                                                    "AWS_ACCOUNT_ID", BuildEnvironmentVariable.builder()
-                                                            .type(BuildEnvironmentVariableType.PLAINTEXT)
-                                                            .value(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                                                            .build()
-
-                                            ))
-                                            .build()
-                                    ).collect(Collectors.toList()))
-                                    .build()
+                    .stageName("Build")
+                    .actions(items.stream().map(server -> CodeBuildAction.Builder.create()
+                            .actionName(server.get("version").getS())
+                            .project(codeBuildDocker)
+                            .input(Artifact.artifact("factorio-docker"))
+                            .type(CodeBuildActionType.BUILD)
+                            .variablesNamespace("factorio-" + server.get("version")
+                                    .getS().replaceAll("\\.", "_"))
+                            .environmentVariables(new TreeMap<>() {{
+                                put("FACTORIO_VERSION",
+                                        BuildEnvironmentVariable.builder()
+                                                .type(BuildEnvironmentVariableType.PLAINTEXT)
+                                                .value(server.get("version").getS())
+                                                .build());
+                                put("IMAGE_REPO_NAME",
+                                        BuildEnvironmentVariable.builder()
+                                                .type(BuildEnvironmentVariableType.PLAINTEXT)
+                                                .value(ecrRepo.getRepositoryName())
+                                                .build());
+                                put("AWS_ACCOUNT_ID", BuildEnvironmentVariable.builder()
+                                        .type(BuildEnvironmentVariableType.PLAINTEXT)
+                                        .value(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                                        .build());
+                            }})
+                            .build()
+                    ).collect(Collectors.toList()))
+                    .build()
             );
         }
 
         codePipeline.addStage(StageOptions.builder()
-                                .stageName("Deploy")
-                                .actions(List.of(
-                                        CodeBuildAction.Builder.create()
-                                                .actionName("Deploy")
-                                                .project(codeBuildCdk)
-                                                .input(Artifact.artifact("factorio-aws"))
-                                                .environmentVariables(Map.of(
-                                                        "DOMAIN_NAME", BuildEnvironmentVariable.builder()
-                                                                .type(BuildEnvironmentVariableType.PLAINTEXT)
-                                                                .value(domainName)
-                                                                .build()
-                                                ))
+                .stageName("Deploy")
+                .actions(List.of(
+                        CodeBuildAction.Builder.create()
+                                .actionName("Deploy")
+                                .project(codeBuildCdk)
+                                .input(Artifact.artifact("factorio-aws"))
+                                .environmentVariables(Collections.singletonMap(
+                                        "DOMAIN_NAME", BuildEnvironmentVariable.builder()
+                                                .type(BuildEnvironmentVariableType.PLAINTEXT)
+                                                .value(domainName)
                                                 .build()
                                 ))
                                 .build()
+                ))
+                .build()
         );
     }
 }
--- src/main/java/io/doush/factorio/FactorioServer.java
+++ src/main/java/io/doush/factorio/FactorioServer.java
@@ -10,11 +10,13 @@ import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps
 import software.amazon.awscdk.services.applicationautoscaling.ScalingInterval;
 import software.amazon.awscdk.services.cloudwatch.Metric;
 import software.amazon.awscdk.services.cloudwatch.MetricProps;
-import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.ec2.ISecurityGroup;
+import software.amazon.awscdk.services.ec2.SubnetSelection;
+import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecs.*;
-import software.amazon.awscdk.services.ecs.Protocol;
-import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.iam.IRole;
+import software.amazon.awscdk.services.iam.Role;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.route53.IHostedZone;
 import software.amazon.awscdk.services.s3.BlockPublicAccess;
@@ -22,8 +24,10 @@ import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.BucketEncryption;
 import software.amazon.awscdk.services.s3.BucketProps;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class FactorioServer extends Construct {
     public FactorioServer(@NotNull Construct scope, @NotNull String id,
@@ -58,18 +62,18 @@ public class FactorioServer extends Construct {
         var container = taskDefinition.addContainer("container",
                 ContainerDefinitionOptions.builder()
                         .cpu(512)
-                        .environment(Map.of(
-                                "S3_BUCKET", bucket.getBucketName(),
-                                "HOSTED_ZONE", hostedZone.getHostedZoneId(),
-                                "DOMAIN", subDomain + "." + domainName,
-                                "SERVER_NAME", serverName
-                        ))
+                        .environment(new TreeMap<>() {{
+                            put("S3_BUCKET", bucket.getBucketName());
+                            put("HOSTED_ZONE", hostedZone.getHostedZoneId());
+                            put("DOMAIN", subDomain + "." + domainName);
+                            put("SERVER_NAME", serverName);
+                        }})
                         .essential(true)
                         .healthCheck(HealthCheck.builder()
                                 .command(List.of("CMD-SHELL", "cat /opt/factorio/server.pid"))
                                 .build()
                         )
-                        .dockerLabels(Map.of(
+                        .dockerLabels(Collections.singletonMap(
                                 "version", version
                         ))
                         .image(containerImage)
